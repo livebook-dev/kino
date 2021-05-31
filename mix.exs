ac6ec8dfb5471f55cea3f3e754564868fe9e53d4
Jonatan Kłosko <jonatanklosko@gmail.com>
2021-05-31 12:44:19 +0200

    Setup docs

--- mix.exs
+++ mix.exs
@@ -1,13 +1,20 @@
 defmodule Kino.MixProject do
   use Mix.Project
 
+  @version "0.1.0"
+  @description "Interactive widgets for Livebook"
+
   def project do
     [
       app: :kino,
-      version: "0.1.0",
+      version: @version,
+      description: @description,
+      name: "Kino",
       elixir: "~> 1.12",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
+      docs: docs(),
+      package: package(),
       xref: [exclude: [VegaLite]]
     ]
   end
@@ -20,7 +27,28 @@ defmodule Kino.MixProject do
 
   defp deps do
     [
-      {:vega_lite, "~> 0.1.0", only: [:dev, :test]}
+      {:vega_lite, "~> 0.1.0", only: [:dev, :test]},
+      {:ex_doc, "~> 0.24", only: :dev, runtime: false}
+    ]
+  end
+
+  defp docs do
+    [
+      main: "Kino",
+      source_url: "https://github.com/elixir-nx/kino",
+      source_ref: "v#{@version}",
+      extras: [
+        {:"README.md", [title: "README"]}
+      ]
+    ]
+  end
+
+  def package do
+    [
+      licenses: ["Apache-2.0"],
+      links: %{
+        "GitHub" => "https://github.com/elixir-nx/kino"
+      }
     ]
   end
 end
