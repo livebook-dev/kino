529a25d09c38dd15d4c1648b2d49a87b771ba0e2
José Valim <jose.valim@dashbit.co>
2021-06-16 11:05:55 +0200

    Update introductory docs

--- lib/kino.ex
+++ lib/kino.ex
@@ -1,41 +1,47 @@
 defmodule Kino do
   @moduledoc """
-  Interactive widgets for Livebook.
+  Client-driven interactive widgets for Livebook.
+
+  Kino is the library used by Livebook to render rich and interactive
+  output directly from your Elixir code.
+
+  Kino renders any data structure that implements the `Kino.Render`
+  protocol, falling back to the `inspect/2` representation whenever
+  an implementation is not available. The data structures supported
+  by Kino out of the box are:
+
+  ### [VegaLite](https://github.com/elixir-nx/vega_lite) widgets
+
+      Vl.new(...)
+      |> Vl.data_from_series(...)
+      |> ...
+
+  ### [Kino.VegaLite](https://github.com/elixir-nx/vega_lite) widgets
+
+  `Kino.VegaLite` is an extension of `VegaLite` that allows data to
+  be streamed:
+
+      widget =
+        Vl.new(...)
+        |> Vl.data_from_series(...)
+        |> ...
+        |> Kino.VegaLite.start()
+        |> Kino.render()
+
+      Kino.VegaLite.push(widget, %{x: 1, y: 2})
+
+  ### All others
+
+  All other data structures are rendered as text using Elixir's
+  `inspect/2`.
   """
 
   @doc """
   Sends the given term as cell output.
 
-  You can think of this function as a generalized
-  `IO.puts/2` that works for any type and is rendered
-  by Livebook similarly to regular evaluation results.
-
-  ## Examples
-
-  Arbitrary data structure
-
-      Kino.render([%{name: "Jake Peralta"}, %{name: "Amy Santiago"}])
-
-  VegaLite plot
-
-      Vl.new(...)
-      |> Vl.data_from_series(...)
-      |> ...
-      |> Kino.render()
-
-      # more code
-
-  Widgets
-
-      vl_widget =
-        Vl.new(...)
-        |> Vl.data_from_series(...)
-        |> ...
-        |> Kino.VegaLite.start()
-
-      Kino.render(vl_widget)
-
-      # stream data to the plot
+  This allows any Livebook cell to have multiple evaluation
+  results. You can think of this function as a generalized
+  `IO.puts/2` that works for any type.
   """
   @spec render(term()) :: term()
   def render(term) do
