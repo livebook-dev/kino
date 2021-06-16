b9c1ca77f25a929d3d365e3cd4e5d4d12ab1f008
Jonatan Kłosko <jonatanklosko@gmail.com>
2021-06-17 00:28:01 +0200

    Automatically render table identifiers as dynamic tables

--- lib/kino/render.ex
+++ lib/kino/render.ex
@@ -32,6 +32,33 @@ defimpl Kino.Render, for: Kino.ETS do
   end
 end
 
+# Elixir built-ins
+
+defimpl Kino.Render, for: Reference do
+  def to_livebook(reference) do
+    cond do
+      accessible_ets_table?(reference) ->
+        reference |> Kino.ETS.start() |> Kino.Render.to_livebook()
+
+      true ->
+        Kino.Output.inspect(reference)
+    end
+  end
+
+  defp accessible_ets_table?(reference) when is_reference(reference) do
+    try do
+      case :ets.info(reference, :protection) do
+        :undefined -> false
+        :private -> false
+        _ -> true
+      end
+    rescue
+      # When the reference is not a valid table identifier
+      ArgumentError -> false
+    end
+  end
+end
+
 # External packages
 
 defimpl Kino.Render, for: VegaLite do
