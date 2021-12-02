6706bdb9e6e1eefd90e5941316581c4fa0b9545f
Jonatan Kłosko <jonatanklosko@gmail.com>
2021-12-02 15:57:48 +0100

    Optimise subscription manager

--- lib/kino/subscription_manager.ex
+++ lib/kino/subscription_manager.ex
@@ -9,8 +9,12 @@ defmodule Kino.SubscriptionManager do
   @name __MODULE__
 
   @type state :: %{
-          subscribers_by_topic: %{(topic :: term()) => {teraget :: pid(), tag :: term()}}
+          topic_with_subscribers: %{topic() => list({pid(), tag()})},
+          pid_with_topics: %{pid() => list(topic())}
         }
+
+  @type topic :: term()
+  @type tag :: term()
 
   def cross_node_name() do
     {@name, node()}
@@ -44,7 +48,7 @@ defmodule Kino.SubscriptionManager do
 
   @impl true
   def init(_opts) do
-    {:ok, %{subscribers_by_topic: %{}}}
+    {:ok, %{topic_with_subscribers: %{}, pid_with_topics: %{}}}
   end
 
   @impl true
@@ -52,9 +56,15 @@ defmodule Kino.SubscriptionManager do
     Process.monitor(pid)
 
     state =
-      update_in(state.subscribers_by_topic[topic], fn
+      update_in(state.topic_with_subscribers[topic], fn
         nil -> [{pid, tag}]
         subscribers -> [{pid, tag} | remove_pid(subscribers, pid)]
+      end)
+
+    state =
+      update_in(state.pid_with_topics[pid], fn
+        nil -> [topic]
+        topics -> if topic in topics, do: topics, else: [topic | topics]
       end)
 
     {:noreply, state}
@@ -62,17 +72,16 @@ defmodule Kino.SubscriptionManager do
 
   def handle_cast({:unsubscribe, topic, pid}, state) do
     state =
-      update_in(state.subscribers_by_topic[topic], fn
-        nil -> nil
-        subscribers -> remove_pid(subscribers, pid)
-      end)
+      state
+      |> remove_topic_subscriber(topic, pid)
+      |> remove_pid_topic(pid, topic)
 
     {:noreply, state}
   end
 
   @impl true
   def handle_info({:event, topic, event}, state) do
-    for {pid, tag} <- state.subscribers_by_topic[topic] || [] do
+    for {pid, tag} <- state.topic_with_subscribers[topic] || [] do
       send(pid, {tag, event})
     end
 
@@ -80,18 +89,46 @@ defmodule Kino.SubscriptionManager do
   end
 
   def handle_info({:clear_topic, topic}, state) do
-    {_, state} = pop_in(state.subscribers_by_topic[topic])
+    {subscribers, state} = pop_in(state.topic_with_subscribers[topic])
+
+    state =
+      Enum.reduce(subscribers || [], state, fn {pid, _tag}, state ->
+        remove_pid_topic(state, pid, topic)
+      end)
+
     {:noreply, state}
   end
 
   def handle_info({:DOWN, _ref, :process, pid, _reason}, state) do
-    subscribers_by_topic =
-      state.subscribers_by_topic
-      |> Enum.map(fn {topic, subscribers} -> {topic, remove_pid(subscribers, pid)} end)
-      |> Enum.filter(&match?({_, []}, &1))
-      |> Map.new()
+    {topics, state} = pop_in(state.pid_with_topics[pid])
 
-    {:noreply, %{state | subscribers_by_topic: subscribers_by_topic}}
+    state =
+      Enum.reduce(topics || [], state, fn topic, state ->
+        remove_topic_subscriber(state, topic, pid)
+      end)
+
+    {:noreply, state}
+  end
+
+  defp remove_topic_subscriber(state, topic, pid) do
+    case pop_in(state.topic_with_subscribers[topic]) do
+      {nil, state} ->
+        state
+
+      {[{^pid, _tag}], state} ->
+        state
+
+      {subscribers, state} ->
+        put_in(state.topic_with_subscribers[topic], remove_pid(subscribers, pid))
+    end
+  end
+
+  defp remove_pid_topic(state, pid, topic) do
+    case pop_in(state.pid_with_topics[pid]) do
+      {nil, state} -> state
+      {[^topic], state} -> state
+      {topics, state} -> put_in(state.pid_with_topics[pid], topics -- [topic])
+    end
   end
 
   defp remove_pid(subscribers, pid) do
