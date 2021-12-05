3478437b154f21e2579f23883bbf16cef101d0ca
Jonatan Kłosko <jonatanklosko@gmail.com>
2021-12-05 15:53:26 +0100

    Assert Kino.inspect/2 sends the expected output

--- test/kino_test.exs
+++ test/kino_test.exs
@@ -1,6 +1,24 @@
 defmodule KinoTest do
   use ExUnit.Case, async: true
 
+  describe "inspect/2" do
+    test "sends a text output to the group leader" do
+      gl =
+        spawn(fn ->
+          assert_receive {:io_request, from, ref, {:livebook_put_output, output}}
+          send(from, {:io_reply, ref, :ok})
+
+          assert {:text, "\e[34m:hey\e[0m"} = output
+        end)
+
+      Process.group_leader(self(), gl)
+
+      Kino.inspect(:hey)
+
+      await_process(gl)
+    end
+  end
+
   describe "start_child/1" do
     test "raises in a process already started as a child" do
       {:ok, pid} =
@@ -13,9 +31,12 @@ defmodule KinoTest do
            end}
         )
 
-      ref = Process.monitor(pid)
-
-      assert_receive {:DOWN, ^ref, :process, _object, _reason}
+      await_process(pid)
     end
+  end
+
+  defp await_process(pid) do
+    ref = Process.monitor(pid)
+    assert_receive {:DOWN, ^ref, :process, _object, _reason}
   end
 end
