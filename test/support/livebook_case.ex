3ae40a3e0063f6402308c2cdc10202ac6f8a6106
Jonatan Kłosko <jonatanklosko@gmail.com>
2022-01-07 13:15:25 +0100

    Simplify group leader setup

--- test/support/livebook_case.ex
+++ test/support/livebook_case.ex
@@ -2,13 +2,8 @@ defmodule Kino.LivebookCase do
   use ExUnit.CaseTemplate
 
   setup do
-    original_gl = Process.group_leader()
-
     gl = start_supervised!({KinoTest.GroupLeader, self()})
     Process.group_leader(self(), gl)
-
-    on_exit(fn ->
-      Process.group_leader(self(), original_gl)
-    end)
+    :ok
   end
 end
