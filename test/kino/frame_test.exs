0707a8323e1a9d1c1ec8c69a6deab9432f40d0e8
Jonatan Kłosko <jonatanklosko@gmail.com>
2022-01-12 18:51:32 +0100

    Fix tests

--- test/kino/frame_test.exs
+++ test/kino/frame_test.exs
@@ -1,39 +1,39 @@
 defmodule Kino.FrameTest do
-  use ExUnit.Case, async: true
+  use Kino.LivebookCase, async: true
 
-  test "render/2 formats the given value into output and sends to the client" do
+  test "render/2 formats the given value into output and sends as :replace frame" do
     widget = Kino.Frame.new()
 
-    connect_self(widget)
-
     Kino.Frame.render(widget, 1)
-    assert_receive {:render, %{output: {:text, "\e[34m1\e[0m"}}}
+    assert_output({:frame, [{:text, "\e[34m1\e[0m"}], %{type: :replace}})
 
     Kino.Frame.render(widget, Kino.Markdown.new("_hey_"))
-    assert_receive {:render, %{output: {:markdown, "_hey_"}}}
+    assert_output({:frame, [{:markdown, "_hey_"}], %{type: :replace}})
+  end
+
+  test "append/2 formats the given value into output and sends as :append frame" do
+    widget = Kino.Frame.new()
+
+    Kino.Frame.append(widget, 1)
+    assert_output({:frame, [{:text, "\e[34m1\e[0m"}], %{type: :append}})
   end
 
   test "periodically/4 evaluates the given callback in background until stopped" do
     widget = Kino.Frame.new()
 
-    connect_self(widget)
+    parent = self()
 
     Kino.Frame.periodically(widget, 1, 1, fn n ->
       if n < 3 do
-        Kino.Frame.render(widget, n)
+        send(parent, {:iteration, n})
         {:cont, n + 1}
       else
         :halt
       end
     end)
 
-    assert_receive {:render, %{output: {:text, "\e[34m1\e[0m"}}}
-    assert_receive {:render, %{output: {:text, "\e[34m2\e[0m"}}}
-    refute_receive {:render, _}, 5
-  end
-
-  defp connect_self(widget) do
-    send(widget.pid, {:connect, self()})
-    assert_receive {:connect_reply, %{}}
+    assert_receive {:iteration, 1}
+    assert_receive {:iteration, 2}
+    refute_receive {:iteration, 3}, 5
   end
 end
--- test/support/livebook_case.ex
+++ test/support/livebook_case.ex
@@ -1,6 +1,12 @@
 defmodule Kino.LivebookCase do
   use ExUnit.CaseTemplate
 
+  using do
+    quote do
+      import KinoTest.Livebook
+    end
+  end
+
   setup do
     gl = start_supervised!({KinoTest.GroupLeader, self()})
     Process.group_leader(self(), gl)
