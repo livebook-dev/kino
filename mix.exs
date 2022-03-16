069a4ee4c5fdecd6c3a8c00495cc8bfa58fde018
Jonatan Kłosko <jonatanklosko@gmail.com>
2022-03-16 23:58:23 +0100

    Add SQL query smart cell

--- mix.exs
+++ mix.exs
@@ -16,7 +16,7 @@ defmodule Kino.MixProject do
       deps: deps(),
       docs: docs(),
       package: package(),
-      xref: [exclude: [VegaLite, Ecto.Query, Ecto.Queryable]]
+      xref: [exclude: [VegaLite, Ecto.Query, Ecto.Queryable, DBConnection]]
     ]
   end
 
@@ -34,6 +34,7 @@ defmodule Kino.MixProject do
     [
       {:vega_lite, "~> 0.1.0", optional: true},
       {:ecto, "~> 3.0", optional: true},
+      {:db_connection, "~> 2.4.2", optional: true},
       {:ex_doc, "~> 0.24", only: :dev, runtime: false}
     ]
   end
