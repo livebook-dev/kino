5da63d510ca3b6534bff6ccb71c590a9b02d3e68
Jonatan Kłosko <jonatanklosko@gmail.com>
2022-03-22 18:31:37 +0100

    Ignore connections to JS data store when there is no matching data

--- test/kino/js/data_store_test.exs
+++ test/kino/js/data_store_test.exs
@@ -12,13 +12,6 @@ defmodule Kino.JS.DataStoreTest do
     assert_receive {:connect_reply, ^data, %{ref: ^ref}}
   end
 
-  test "replies to connect messages with nil when no matching data is found" do
-    ref = Kino.Output.random_ref()
-
-    send(DataStore, {:connect, self(), %{origin: self(), ref: ref}})
-    assert_receive {:connect_reply, nil, %{ref: ^ref}}
-  end
-
   test "{:remove, ref} removes data for the given ref" do
     ref = Kino.Output.random_ref()
     data = [1, 2, 3]
@@ -27,6 +20,6 @@ defmodule Kino.JS.DataStoreTest do
     send(DataStore, {:remove, ref})
 
     send(DataStore, {:connect, self(), %{origin: self(), ref: ref}})
-    assert_receive {:connect_reply, nil, %{ref: ^ref}}
+    refute_receive {:connect_reply, _, %{ref: ^ref}}
   end
 end
