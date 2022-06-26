308a31ab697687601342494071fb3b8e2e645311
Alex Koutmos <akoutmos@gmail.com>
2022-06-26 01:43:17 -0400

    Links are working now

--- lib/kino/process.ex
+++ lib/kino/process.ex
@@ -72,16 +72,20 @@ defmodule Kino.Process do
   """
   @spec generate_supervision_tree(pid()) :: Kino.Markdown.t()
   def generate_supervision_tree(supervisor) when is_pid(supervisor) do
-    initial_pid_lookup = Map.put(%{}, supervisor, 0)
+    initial_pid_lookup = Map.put(%{}, supervisor, {0, :supervisor})
 
+    # TODO: Check that the provided PID is actually a supervisor
     {edges, _, _} =
       supervisor
       |> Supervisor.which_children()
-      |> traverse_processes(supervisor, {[], 1, initial_pid_lookup})
-      |> traverse_links(supervisor)
+      |> traverse_processes(supervisor, {%{}, 1, initial_pid_lookup})
+      |> traverse_links()
 
     edges =
-      Enum.map_join(edges, "\n", fn edge -> SupervisorGraphEdge.generate_mermaid_entry(edge) end)
+      edges
+      |> Enum.map_join("\n", fn {_pid_pair, edge} ->
+        SupervisorGraphEdge.generate_mermaid_entry(edge)
+      end)
 
     Kino.Markdown.new("""
     ```mermaid
@@ -99,40 +103,77 @@ defmodule Kino.Process do
   end
 
   defp traverse_processes([{_, pid, :supervisor, _} | rest], supervisor, {rels, idx, pid_keys}) do
-    pid_keys = Map.put(pid_keys, pid, idx)
+    pid_keys = Map.put(pid_keys, pid, {idx, :supervisor})
     children = Supervisor.which_children(pid)
 
-    supervisor_node =
-      pid_keys
-      |> Map.get(supervisor)
-      |> SupervisorGraphNode.new(supervisor, :supervisor)
+    {supervisor_idx, _type} = Map.get(pid_keys, supervisor)
+    supervisor_node = SupervisorGraphNode.new(supervisor_idx, supervisor, :supervisor)
 
     worker_node = SupervisorGraphNode.new(idx, pid, :supervisor)
-
     new_connection = SupervisorGraphEdge.new(supervisor_node, worker_node, :supervisor)
 
-    {subtree_rels, idx, pid_keys} = traverse_processes(children, pid, {[], idx + 1, pid_keys})
-    traverse_processes(rest, supervisor, {[new_connection | subtree_rels] ++ rels, idx, pid_keys})
+    {subtree_rels, idx, pid_keys} = traverse_processes(children, pid, {%{}, idx + 1, pid_keys})
+
+    updated_rels =
+      rels
+      |> add_rels(subtree_rels)
+      |> add_rel(new_connection)
+
+    traverse_processes(rest, supervisor, {updated_rels, idx, pid_keys})
   end
 
   defp traverse_processes([{_, pid, :worker, _} | rest], supervisor, {rels, idx, pid_keys}) do
-    supervisor_node =
-      pid_keys
-      |> Map.get(supervisor)
-      |> SupervisorGraphNode.new(supervisor, :supervisor)
+    pid_keys = Map.put(pid_keys, pid, {idx, :worker})
+
+    {supervisor_idx, _type} = Map.get(pid_keys, supervisor)
+    supervisor_node = SupervisorGraphNode.new(supervisor_idx, supervisor, :supervisor)
 
     worker_node = SupervisorGraphNode.new(idx, pid, :worker)
-
     new_connection = SupervisorGraphEdge.new(supervisor_node, worker_node, :supervisor)
 
-    traverse_processes(rest, supervisor, {[new_connection | rels], idx + 1, pid_keys})
+    traverse_processes(rest, supervisor, {add_rel(rels, new_connection), idx + 1, pid_keys})
   end
 
   defp traverse_processes([], _, acc) do
     acc
   end
 
-  defp traverse_links({rels, _idx, pid_keys} = thing, supervisor) do
-    thing
+  defp add_rels(rels, additional_rels) do
+    Map.merge(rels, additional_rels, fn _key, edge_1, _edge_2 -> edge_1 end)
+  end
+
+  defp add_rel(rels, %SupervisorGraphEdge{} = edge) do
+    lookup = Enum.sort([edge.node_1.pid, edge.node_2.pid])
+
+    Map.put_new(rels, lookup, edge)
+  end
+
+  defp traverse_links({rels, idx, pid_keys}) do
+    rels_with_links =
+      pid_keys
+      |> Enum.reduce(rels, fn {pid, _idx}, rels_with_links ->
+        {:links, links} = Process.info(pid, :links)
+
+        links
+        |> Enum.reduce(rels_with_links, fn link, acc ->
+          add_new_links_to_acc(pid_keys, pid, link, acc)
+        end)
+      end)
+
+    {rels_with_links, idx, pid_keys}
+  end
+
+  defp add_new_links_to_acc(pid_keys, pid, link_pid, acc) do
+    with {:ok, {idx_1, type_1}} <- Map.fetch(pid_keys, pid),
+         {:ok, {idx_2, type_2}} <- Map.fetch(pid_keys, link_pid) do
+      process_1 = SupervisorGraphNode.new(idx_1, pid, type_1)
+      process_2 = SupervisorGraphNode.new(idx_2, link_pid, type_2)
+      link_edge = SupervisorGraphEdge.new(process_1, process_2, :link)
+
+      add_rel(acc, link_edge)
+    else
+      _ ->
+        acc
+    end
   end
 end
