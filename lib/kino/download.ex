3da20e7be80fcd46a63e5160b6952d229a2dde2f
Tyler Pachal <tylerpachalandfriends@gmail.com>
2022-07-27 17:09:07 -0300

    Switch to Kino.JS.Live

--- lib/kino/download.ex
+++ lib/kino/download.ex
@@ -1,45 +1,83 @@
 defmodule Kino.Download do
-  @moduledoc """
-  Built on top of `Kino.JS`, uses Javascript to initiate a download
-  on the client's machine.
-  '''
-  use Kino.JS
+  @moduledoc false
 
-  @doc ~S'''
-  Initiates a download on the client's machine.  The content is
-  generated lazily by invoking the `content_fun` argument.
+  use Kino.JS
+  use Kino.JS.Live
+
+  @doc """
+  Initiates a download.  The content is generated lazily by invoking
+  the `content_fun` argument on the server when the "Download" button
+  is clicked.
 
   ## Examples
 
   Passing the content from an existing variable:
 
       json = ~s/{"foo": "bar"}/
-      Kino.Download.start("file.json", fn -> json end)
+      Kino.Download.new("file.json", fn -> json end)
 
   Lazily generating a large file:
 
-      Kino.Download.start("large-file.txt", fn ->
+      Kino.Download.new("large-file.txt", fn ->
         String.duplicate("data ", 10_000_000)
       end)
-  '''
-  @spec start(String.t(), (() -> String.t())) :: Kino.JS.t()
-  def start(filename, content_fun) do
-    data = %{
-      content: content_fun.(),
-      filename: filename
-    }
+  """
+  def new(filename, content_fun) do
+    Kino.JS.Live.new(__MODULE__, {filename, content_fun})
+  end
 
-    Kino.JS.new(__MODULE__, data)
+  @impl true
+  def init({filename, content_fun}, ctx) do
+    {:ok, assign(ctx, filename: filename, content_fun: content_fun)}
+  end
+
+  @impl true
+  def handle_connect(ctx) do
+    {:ok, Map.take(ctx.assigns, [:filename]), ctx}
+  end
+
+  @impl true
+  def handle_event("prepare_download", %{"filename" => filename}, ctx) do
+    ctx = assign(ctx, filename: filename)
+
+    # To keep things generic the content is a base64-encoded binary
+    file_content =
+      ctx.assigns.content_fun.()
+      |> Base.encode64()
+
+    reply_payload = {:binary, %{filename: ctx.assigns.filename}, file_content}
+
+    broadcast_event(ctx, "do_download", reply_payload)
+
+    {:noreply, ctx}
   end
 
   asset "main.js" do
     """
     export function init(ctx, data) {
-      var hiddenElement = document.createElement('a');
-      hiddenElement.href = 'data:attachment,' + encodeURI(data.content);
-      hiddenElement.target = '_blank';
-      hiddenElement.download = data.filename;
-      hiddenElement.click();
+
+      // Handle the event which initiates the actual downloading of the file
+      ctx.handleEvent("do_download", ([info, arrayBuffer]) => {
+        const decoder = new TextDecoder("utf-8");
+        const content = decoder.decode(arrayBuffer);
+
+        // Create a hidden element that we can attach the download to
+        const hiddenElement = document.createElement('a');
+        hiddenElement.href = `data:attachment/application/octet-stream;base64,${content}`;
+        hiddenElement.target = '_blank';
+        hiddenElement.download = info.filename;
+        hiddenElement.click();
+      });
+
+      // Create the Download button
+      const button = document.createElement('button');
+      button.innerHTML = `Download ${data.filename}`;
+      button.addEventListener('click', buttonClicked);
+      ctx.root.appendChild(button);
+
+      function buttonClicked() {
+        ctx.pushEvent("prepare_download", {filename: data.filename})
+      }
     }
     """
   end
--- test/kino/download_test.exs
+++ test/kino/download_test.exs
@@ -1,10 +1,7 @@
 defmodule Kino.DownloadTest do
   use ExUnit.Case, async: true
 
-  describe "start/3" do
-    test "returns %Kino.JS{} " do
-      assert %Kino.JS{module: Kino.Download} =
-               Kino.Download.start("data.json", fn -> ~s/{"foo" => "bar"}/ end)
-    end
+  describe "new/2" do
+    # TODO
   end
 end
