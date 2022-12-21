36197a2821e37e2e5435e9355f215c9de806a61e
Cristine Guadelupe <cristineguadelupe@me.com>
2022-12-21 17:36:30 -0300

    Removes unused css

--- assets/data_table/main.js
+++ assets/data_table/main.js
@@ -79,8 +79,10 @@ const filtering = {
 export function init(ctx, data) {
   ctx.importCSS("main.css");
   ctx.importCSS(
-    "https://fonts.googleapis.com/css2?family=JetBrains+Mono:wght@400;500;600&display=swap",
     "https://fonts.googleapis.com/css2?family=Inter:wght@400;500&display=swap"
+  );
+  ctx.importCSS(
+    "https://fonts.googleapis.com/css2?family=JetBrains+Mono:wght@400;500;600&display=swap"
   );
 
   const root = createRoot(ctx.root);
