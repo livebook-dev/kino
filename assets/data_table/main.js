e5f495381e7e1a462e65e0e5a939f72d177cfb9a
Cristine Guadelupe <cristineguadelupe@me.com>
2022-12-24 16:17:10 -0300

    Closes header menu on sorting

--- assets/data_table/main.js
+++ assets/data_table/main.js
@@ -318,11 +318,8 @@ function App({ ctx, data }) {
 
   const orderBy = (order) => {
     const key = order !== "none" ? columns[menu.column].id : null;
-    setCurrentMenuForm({
-      ...currentMenuForm,
-      sort: { order: order, key: key },
-    });
     ctx.pushEvent("order_by", { key, order: order ?? "asc" });
+    setMenu(null);
   };
 
   const resetFilters = () => {
