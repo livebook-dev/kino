832d09b665bb1be187e674fcc9dfbdf889bec2a2
Jonatan Kłosko <jonatanklosko@gmail.com>
2023-06-06 11:56:14 +0200

    Don't use inline assets for built-in kinos

--- lib/kino/html.ex
+++ lib/kino/html.ex
@@ -29,7 +29,7 @@ defmodule Kino.HTML do
 
   '''
 
-  use Kino.JS
+  use Kino.JS, assets_path: "lib/assets/html"
 
   @type t :: Kino.JS.t()
 
@@ -39,36 +39,5 @@ defmodule Kino.HTML do
   @spec new(String.t()) :: t()
   def new(html) when is_binary(html) do
     Kino.JS.new(__MODULE__, html)
-  end
-
-  asset "main.js" do
-    """
-    export function init(ctx, html) {
-      setInnerHTML(ctx.root, html);
-    }
-
-    function setInnerHTML(element, html) {
-      // By default setting inner HTML doesn't execute scripts, as
-      // noted in [1], however we can work around this by explicitly
-      // building the script element.
-      //
-      // [1]: https://developer.mozilla.org/en-US/docs/Web/API/Element/innerHTML#security_considerations
-
-      element.innerHTML = html;
-
-      Array.from(element.querySelectorAll("script")).forEach((scriptEl) => {
-        const safeScriptEl = document.createElement("script");
-
-        Array.from(scriptEl.attributes).forEach((attr) => {
-          safeScriptEl.setAttribute(attr.name, attr.value)
-        });
-
-        const scriptText = document.createTextNode(scriptEl.innerHTML);
-        safeScriptEl.appendChild(scriptText);
-
-        scriptEl.parentNode.replaceChild(safeScriptEl, scriptEl);
-      });
-    }
-    """
   end
 end
--- lib/kino/mermaid.ex
+++ lib/kino/mermaid.ex
@@ -20,7 +20,7 @@ defmodule Kino.Mermaid do
 
   '''
 
-  use Kino.JS
+  use Kino.JS, assets_path: "lib/assets/mermaid"
 
   @type t :: Kino.JS.t()
 
@@ -30,33 +30,5 @@ defmodule Kino.Mermaid do
   @spec new(binary()) :: t()
   def new(content) do
     Kino.JS.new(__MODULE__, content, export_info_string: "mermaid")
-  end
-
-  asset "main.js" do
-    """
-    import "https://cdn.jsdelivr.net/npm/mermaid@9.1.3/dist/mermaid.min.js";
-
-    mermaid.initialize({ startOnLoad: false });
-
-    export function init(ctx, content) {
-      function render() {
-        mermaid.render("graph1", content, (svgSource, bindListeners) => {
-          ctx.root.innerHTML = svgSource;
-          bindListeners && bindListeners(ctx.root);
-
-          // A workaround for https://github.com/mermaid-js/mermaid/issues/1758
-          const svg = ctx.root.querySelector("svg");
-          svg.removeAttribute("height");
-        });
-      }
-
-      // If the JS view is not visible, defer initialization until it becomes visible
-      if (window.innerWidth === 0) {
-        window.addEventListener("resize", () => render(), { once: true });
-      } else {
-        render();
-      }
-    }
-    """
   end
 end
