d9a0285d063332d93e50ae4a8d1402927efbb353
Cristine Guadelupe <cristineguadelupe@me.com>
2023-09-04 21:33:08 +0700

    Remote cell (#328)

---
+++ lib/kino/remote_cell.ex
@@ -0,0 +1,86 @@
+defmodule Kino.RemoteCell do
+  @moduledoc false
+
+  use Kino.JS, assets_path: "lib/assets/remote_cell"
+  use Kino.JS.Live
+  use Kino.SmartCell, name: "Remote cell"
+
+  @default_code ":ok"
+
+  @impl true
+  def init(attrs, ctx) do
+    fields = %{
+      "assign_to" => attrs["assign_to"] || "",
+      "cookie" => attrs["cookie"] || "",
+      "node" => attrs["node"] || ""
+    }
+
+    ctx = assign(ctx, fields: fields)
+
+    {:ok, ctx, editor: [attribute: "code", language: "elixir", default_source: @default_code]}
+  end
+
+  @impl true
+  def handle_connect(ctx) do
+    payload = %{fields: ctx.assigns.fields}
+    {:ok, payload, ctx}
+  end
+
+  @impl true
+  def handle_event("update_field", %{"field" => field, "value" => value}, ctx) do
+    ctx = update(ctx, :fields, &Map.put(&1, field, value))
+    broadcast_event(ctx, "update_field", %{"fields" => %{field => value}})
+
+    {:noreply, ctx}
+  end
+
+  @impl true
+  def to_attrs(ctx) do
+    ctx.assigns.fields
+  end
+
+  @impl true
+  def to_source(%{"node" => ""}), do: ""
+  def to_source(%{"cookie" => ""}), do: ""
+  def to_source(%{"code" => ""}), do: ""
+
+  def to_source(%{"code" => code} = attrs) do
+    code = Code.string_to_quoted(code)
+    to_source(attrs, code)
+  end
+
+  defp to_source(%{"node" => node, "cookie" => cookie, "assign_to" => var}, {:ok, code}) do
+    var = if Kino.SmartCell.valid_variable_name?(var), do: var
+    call = build_call(code) |> build_var(var)
+
+    quote do
+      node = unquote(String.to_atom(node))
+      Node.set_cookie(node, unquote(String.to_atom(cookie)))
+      unquote(call)
+    end
+    |> Kino.SmartCell.quoted_to_string()
+  end
+
+  defp to_source(%{"code" => code}, {:error, _reason}) do
+    "# Invalid code for RPC, reproducing the error below\n" <>
+      Kino.SmartCell.quoted_to_string(
+        quote do
+          Code.string_to_quoted!(unquote(code))
+        end
+      )
+  end
+
+  defp build_call(code) do
+    quote do
+      :erpc.call(node, fn -> unquote(code) end)
+    end
+  end
+
+  defp build_var(call, nil), do: call
+
+  defp build_var(call, var) do
+    quote do
+      unquote({String.to_atom(var), [], nil}) = unquote(call)
+    end
+  end
+end
---
+++ test/kino/remote_cell_test.exs
@@ -0,0 +1,123 @@
+defmodule Kino.RemoteCellTest do
+  use ExUnit.Case, async: true
+
+  import Kino.Test
+
+  alias Kino.RemoteCell
+
+  setup :configure_livebook_bridge
+
+  @fields %{
+    "node" => "name@node",
+    "cookie" => "node-cookie",
+    "assign_to" => "",
+    "code" => ":ok"
+  }
+
+  test "returns the defaults when starting fresh with no data" do
+    {_kino, source} = start_smart_cell!(RemoteCell, %{})
+
+    assert source == ""
+  end
+
+  test "from saved attrs" do
+    {_kino, source} = start_smart_cell!(RemoteCell, @fields)
+
+    assert source == """
+           node = :name@node
+           Node.set_cookie(node, :"node-cookie")
+           :erpc.call(node, fn -> :ok end)\
+           """
+  end
+
+  test "from saved attrs with result" do
+    attrs = %{@fields | "assign_to" => "result"}
+    {_kino, source} = start_smart_cell!(RemoteCell, attrs)
+
+    assert source == """
+           node = :name@node
+           Node.set_cookie(node, :"node-cookie")
+           result = :erpc.call(node, fn -> :ok end)\
+           """
+  end
+
+  describe "code generation" do
+    test "do not generate code when there's no node" do
+      attrs = %{@fields | "node" => ""}
+      assert RemoteCell.to_source(attrs) == ""
+    end
+
+    test "do not generate code when there's no cookie" do
+      attrs = %{@fields | "cookie" => ""}
+      assert RemoteCell.to_source(attrs) == ""
+    end
+
+    test "do not generate code when there's no code" do
+      attrs = %{@fields | "code" => ""}
+      assert RemoteCell.to_source(attrs) == ""
+    end
+
+    test "emites Code.string_to_quoted! when the code is invalid" do
+      attrs = %{@fields | "code" => "1 + "}
+
+      assert RemoteCell.to_source(attrs) == """
+             # Invalid code for RPC, reproducing the error below
+             Code.string_to_quoted!("1 + ")\
+             """
+    end
+
+    test "generates an erpc call when there's valid code" do
+      code1 = %{@fields | "code" => "1 + 1"}
+      code2 = %{@fields | "code" => "1 == 1"}
+      code3 = %{@fields | "code" => "a = 1\na + a"}
+
+      assert RemoteCell.to_source(@fields) == """
+             node = :name@node
+             Node.set_cookie(node, :"node-cookie")
+             :erpc.call(node, fn -> :ok end)\
+             """
+
+      assert RemoteCell.to_source(code1) == """
+             node = :name@node
+             Node.set_cookie(node, :"node-cookie")
+             :erpc.call(node, fn -> 1 + 1 end)\
+             """
+
+      assert RemoteCell.to_source(code2) == """
+             node = :name@node
+             Node.set_cookie(node, :"node-cookie")
+             :erpc.call(node, fn -> 1 == 1 end)\
+             """
+
+      assert RemoteCell.to_source(code3) == """
+             node = :name@node
+             Node.set_cookie(node, :"node-cookie")
+
+             :erpc.call(node, fn ->
+               a = 1
+               a + a
+             end)\
+             """
+    end
+
+    test "assign to a variable" do
+      attrs = %{@fields | "assign_to" => "result"}
+
+      assert RemoteCell.to_source(attrs) == """
+             node = :name@node
+             Node.set_cookie(node, :"node-cookie")
+             result = :erpc.call(node, fn -> :ok end)\
+             """
+    end
+
+    test "do not assign to an invalid variable" do
+      attrs = %{@fields | "assign_to" => "invalid result"}
+
+      assert RemoteCell.to_source(attrs) == """
+             node = :name@node
+             Node.set_cookie(node, :"node-cookie")
+             :erpc.call(node, fn -> :ok end)\
+             """
+    end
+  end
+end
