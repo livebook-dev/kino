6f47ed438ae2dff9629cfdbf505ad4394bfba7c0
Cristine Guadelupe <cristineguadelupe@me.com>
2023-09-04 14:58:53 +0700

    To code

--- lib/kino/remote_cell.ex
+++ lib/kino/remote_cell.ex
@@ -10,9 +10,9 @@ defmodule Kino.RemoteCell do
   @impl true
   def init(attrs, ctx) do
     fields = %{
-      "assign_to" => attrs["assign_to"],
-      "cookie" => attrs["cookie"],
-      "node" => attrs["node"]
+      "assign_to" => attrs["assign_to"] || "",
+      "cookie" => attrs["cookie"] || "",
+      "node" => attrs["node"] || ""
     }
 
     ctx = assign(ctx, fields: fields)
@@ -44,8 +44,40 @@ defmodule Kino.RemoteCell do
     attrs |> to_quoted() |> Kino.SmartCell.quoted_to_string()
   end
 
-  defp to_quoted(_ctx) do
+  defp to_quoted(%{"node" => ""}) do
     quote do
+    end
+  end
+
+  defp to_quoted(%{"cookie" => ""}) do
+    quote do
+    end
+  end
+
+  defp to_quoted(%{"code" => ""}) do
+    quote do
+    end
+  end
+
+  defp to_quoted(%{"code" => code} = attrs) do
+    code = Code.string_to_quoted(code)
+    to_quoted(attrs, code)
+  end
+
+  defp to_quoted(%{"node" => node, "cookie" => cookie}, {:ok, code}) do
+    quote do
+      node = unquote(String.to_atom(node))
+      cookie = unquote(String.to_atom(cookie))
+      Node.set_cookie(node, cookie)
+      Node.connect(node)
+
+      :erpc.call(node, fn -> unquote(code) end)
+    end
+  end
+
+  defp to_quoted(%{"code" => code}, {:error, _reason}) do
+    quote do
+      Code.string_to_quoted!(unquote(code))
     end
   end
 end
