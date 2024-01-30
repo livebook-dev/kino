f8802d0a2b0b4d62a0fa9a086cfd31bf089ed5ba
Jonatan Kłosko <jonatanklosko@gmail.com>
2024-01-30 09:59:30 +0100

    Simplify secret field (#389)

--- assets/remote_execution_cell/main.js
+++ assets/remote_execution_cell/main.js
@@ -14,7 +14,7 @@ export function init(ctx, payload) {
     props: {
       label: {
         type: String,
-        default: "",
+        default: null,
       },
       inputClass: {
         type: String,
@@ -24,10 +24,6 @@ export function init(ctx, payload) {
         type: [String, Number],
         default: "",
       },
-      inline: {
-        type: Boolean,
-        default: false,
-      },
       required: {
         type: Boolean,
         default: false,
@@ -35,17 +31,34 @@ export function init(ctx, payload) {
     },
 
     template: `
-    <div :class="[inline ? 'inline-field' : 'field']">
-      <label :class="inline ? 'inline-input-label' : 'input-label'">
+    <div class="field">
+      <label v-if="label" class="input-label">
         {{ label }}
       </label>
       <input
         :value="modelValue"
         @input="$emit('update:modelValue', $event.target.value)"
         v-bind="$attrs"
-        :class="[inputClass, { required: !modelValue && required }]"
+        :class="[inputClass, { required: !(modelValue || $attrs.value) && required }]"
       >
     </div>
+    `,
+  };
+
+  const InlineLabel = {
+    name: "InlineLabel",
+
+    props: {
+      label: {
+        type: String,
+        default: null,
+      },
+    },
+
+    template: `
+    <label class="inline-label">
+      {{ label }}
+    </label>
     `,
   };
 
@@ -71,7 +84,7 @@ export function init(ctx, payload) {
       },
       label: {
         type: String,
-        default: "",
+        default: null,
       },
       toggleInputValue: {
         type: [String, Number],
@@ -89,17 +102,9 @@ export function init(ctx, payload) {
         type: String,
         default: "Select secret",
       },
-      inline: {
-        type: Boolean,
-        default: false,
-      },
       required: {
         type: Boolean,
         default: false,
-      },
-      iconOffset: {
-        type: String,
-        default: "md",
       },
     },
 
@@ -127,12 +132,11 @@ export function init(ctx, payload) {
           :name="secretInputName"
           :label="label"
           :value="secretInputValue"
-          inputClass="input input-icon"
+          inputClass="input secret-input"
           readonly
           @click="selectSecret"
           @input="$emit('update:secretInputValue', $event.target.value)"
           :required="required"
-          :inline="inline"
         />
         <BaseInput
           v-else
@@ -140,33 +144,30 @@ export function init(ctx, payload) {
           :label="label"
           type="text"
           :value="textInputValue"
-          inputClass="input input-icon-text"
+          inputClass="input"
           @input="$emit('update:textInputValue', $event.target.value)"
           :required="required"
-          :inline="inline"
         />
-        <div :class="['icon-container', { inline: inline }, iconOffset]">
-          <label class="hidden-checkbox">
-            <input
-              type="checkbox"
-              :name="toggleInputName"
-              :checked="toggleInputValue"
-              @input="$emit('update:toggleInputValue', $event.target.checked)"
-              class="hidden-checkbox-input"
-            />
-            <svg v-if="toggleInputValue" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"
-                  width="22" height="22">
-              <path fill="none" d="M0 0h24v24H0z"/>
-              <path d="M18 8h2a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V9a1 1 0 0 1 1-1h2V7a6 6 0 1 1 12 0v1zM5
-                10v10h14V10H5zm6 4h2v2h-2v-2zm-4 0h2v2H7v-2zm8 0h2v2h-2v-2zm1-6V7a4 4 0 1 0-8 0v1h8z" fill="#000"/>
-            </svg>
-            <svg v-else xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24">
-              <path fill="none" d="M0 0h24v24H0z"/>
-              <path d="M21 3v18H3V3h18zm-8.001 3h-2L6.6 17h2.154l1.199-3h4.09l1.201 3h2.155l-4.4-11zm-1 2.885L13.244
-                12h-2.492l1.247-3.115z" fill="#445668"/>
-            </svg>
-          </label>
-        </div>
+        <label class="icon-container">
+          <input
+            type="checkbox"
+            :name="toggleInputName"
+            :checked="toggleInputValue"
+            @input="$emit('update:toggleInputValue', $event.target.checked)"
+            class="hidden-checkbox-input"
+          />
+          <svg v-if="toggleInputValue" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"
+                width="22" height="22">
+            <path fill="none" d="M0 0h24v24H0z"/>
+            <path d="M18 8h2a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V9a1 1 0 0 1 1-1h2V7a6 6 0 1 1 12 0v1zM5
+              10v10h14V10H5zm6 4h2v2h-2v-2zm-4 0h2v2H7v-2zm8 0h2v2h-2v-2zm1-6V7a4 4 0 1 0-8 0v1h8z" fill="#000"/>
+          </svg>
+          <svg v-else xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24">
+            <path fill="none" d="M0 0h24v24H0z"/>
+            <path d="M21 3v18H3V3h18zm-8.001 3h-2L6.6 17h2.154l1.199-3h4.09l1.201 3h2.155l-4.4-11zm-1 2.885L13.244
+              12h-2.492l1.247-3.115z" fill="#445668"/>
+          </svg>
+        </label>
       </div>
     `,
   };
@@ -174,6 +175,7 @@ export function init(ctx, payload) {
   const app = Vue.createApp({
     components: {
       BaseInput,
+      InlineLabel,
       BaseSecret,
     },
 
@@ -181,38 +183,40 @@ export function init(ctx, payload) {
     <div class="app">
       <form @change="handleFieldChange">
         <div class="header">
-          <BaseSecret
-            textInputName="node"
-            secretInputName="node_secret"
-            toggleInputName="use_node_secret"
-            label="Node"
-            v-model:textInputValue="fields.node"
-            v-model:secretInputValue="fields.node_secret"
-            v-model:toggleInputValue="fields.use_node_secret"
-            modalTitle="Set node value"
-            iconOffset="sm"
-            inline
-            required
-          />
-          <BaseSecret
-            textInputName="cookie"
-            secretInputName="cookie_secret"
-            toggleInputName="use_cookie_secret"
-            label="Cookie"
-            v-model:textInputValue="fields.cookie"
-            v-model:secretInputValue="fields.cookie_secret"
-            v-model:toggleInputValue="fields.use_cookie_secret"
-            modalTitle="Set cookie value"
-            inline
-            required
-          />
-          <BaseInput
-            name="assign_to"
-            label="Assign to"
-            v-model="fields.assign_to"
-            inputClass="input input--xs"
-            inline
-          />
+          <div class="field-wrapper">
+            <InlineLabel label="Node" />
+            <BaseSecret
+              textInputName="node"
+              secretInputName="node_secret"
+              toggleInputName="use_node_secret"
+              v-model:textInputValue="fields.node"
+              v-model:secretInputValue="fields.node_secret"
+              v-model:toggleInputValue="fields.use_node_secret"
+              modalTitle="Set node value"
+              required
+            />
+          </div>
+          <div class="field-wrapper">
+            <InlineLabel label="Cookie" />
+            <BaseSecret
+              textInputName="cookie"
+              secretInputName="cookie_secret"
+              toggleInputName="use_cookie_secret"
+              v-model:textInputValue="fields.cookie"
+              v-model:secretInputValue="fields.cookie_secret"
+              v-model:toggleInputValue="fields.use_cookie_secret"
+              modalTitle="Set cookie value"
+              required
+            />
+          </div>
+          <div class="field-wrapper">
+            <InlineLabel label="Assign to" />
+            <BaseInput
+              name="assign_to"
+              v-model="fields.assign_to"
+              inputClass="input input--xs"
+            />
+          </div>
         </div>
       </form>
     </div>
