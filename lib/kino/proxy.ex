21d7f523bae3cbc6a9c6ad775c3a0a518e978e88
Alexandre de Souza <alexandre@aledsz.com.br>
2024-05-24 17:18:11 -0300

    Add docs

--- lib/kino/proxy.ex
+++ lib/kino/proxy.ex
@@ -2,7 +2,53 @@ defmodule Kino.Proxy do
   @moduledoc """
   Functionality for handling proxy requests forwarded from Livebook.
 
-  TODO: Write an extensive docs here.
+  This functionality will make the requests forwarded from Livebook
+  through a proxy handler and send the response to the incoming connection
+  according to user definition.
+
+  The proxy handler supports the routes below to perform this proxy
+  between the `Livebook` and current runtime:
+
+    * `/sessions/:id/proxy/*path` - for notebook sessions.
+
+    * `/apps/:slug/:session_id/proxy/*path` - for app sessions.
+
+  Only certain fields will be forwarded through the proxy handler, which
+  builds a new `%Plug.Conn{}` and sends to the listener function.
+
+    * `:host`
+    * `:method`
+    * `:owner`
+    * `:port`
+    * `:remote_ip`
+    * `:query_string`
+    * `:path_info`
+    * `:scheme`
+    * `:script_name`
+    * `:req_headers`
+
+  It is possible to create notebooks and apps as APIs, allowing the user to fetch the
+  request data and send a proper response.
+
+      data = <<...>>
+      token = "auth-token"
+
+      Kino.Proxy.listen(fn
+        %{path_info: ["export", "data"]} = conn ->
+          ["Bearer " <> ^token] = Plug.Conn.get_req_header(conn, "authorization")
+
+          conn
+          |> Plug.Conn.put_resp_header("content-type", "application/csv")
+          |> Plug.Conn.send_resp(200, data)
+
+        conn ->
+          conn
+          |> Plug.Conn.put_resp_header("content-type", "application/text")
+          |> Plug.Conn.send_resp(200, "use /export/data to get extract the report data")
+      end)
+
+  So you would need to access the `/sessions/:id/proxy/export/data` to extract the data from
+  your session and return as a body response.
   """
 
   @doc """
