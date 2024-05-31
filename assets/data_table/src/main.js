de0abce7f958835a2686103769190d26c02b58fb
Cristine Guadelupe <cristineguadelupe@me.com>
2024-05-31 14:18:45 +0700

    Ensure that the custom font is loaded (#435)

--- assets/data_table/src/main.js
+++ assets/data_table/src/main.js
@@ -5,14 +5,17 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import { App } from "./App";
 
-export function init(ctx, data) {
+export async function init(ctx, data) {
+  // In Firefox and Safari, during the first load (uncached), the data
+  // grid renders the default font and the font is only updated after
+  // hovering the grid. Ensuring the font is loaded helps in Firefox.
+  await ctx.importCSS(
+    "https://fonts.googleapis.com/css2?family=JetBrains+Mono:wght@400;500;600&display=swap"
+  );
+  ctx.importCSS(
+    "https://fonts.googleapis.com/css2?family=Inter:wght@400;500&display=swap"
+  );
   ctx.importCSS("main.css");
-  ctx.importCSS(
-    "https://fonts.googleapis.com/css2?family=Inter:wght@400;500&display=swap",
-  );
-  ctx.importCSS(
-    "https://fonts.googleapis.com/css2?family=JetBrains+Mono:wght@400;500;600&display=swap",
-  );
 
   const root = createRoot(ctx.root);
   root.render(<App ctx={ctx} data={data} />);
