1221c26f58f52482726cb36933561f563dd68e79
Chris Keele <christhekeele@gmail.com>
2024-10-12 14:43:33 -0500

    Add download button to Mermaid SVG.

--- assets/packs/mermaid/main.js
+++ assets/packs/mermaid/main.js
@@ -1,11 +1,39 @@
 import mermaid from "mermaid";
+import "./main.css";
 
 mermaid.initialize({ startOnLoad: false });
 
 export function init(ctx, content) {
+  ctx.importCSS("main.css")
+  
   function render() {
     mermaid.render("graph1", content).then(({ svg, bindFunctions }) => {
-      ctx.root.innerHTML = svg;
+      ctx.root.innerHTML = `
+        <div id="mermaid">
+          ${svg}
+          <button id="download" title="Download">⇩</button>
+        </div>
+      `
+      ctx.root.querySelector("#download").addEventListener("click", (event) => {
+        var binaryData = [];
+        binaryData.push(svg);
+        const downloadBlob = URL.createObjectURL(new Blob(binaryData, {type: "image/svg+xml"}));
+
+        const downloadLink = document.createElement("a");
+        downloadLink.href = downloadBlob;
+        downloadLink.download = "mermaid.svg";
+        document.body.appendChild(downloadLink);
+
+        downloadLink.dispatchEvent(
+          new MouseEvent('click', { 
+            bubbles: true, 
+            cancelable: true, 
+            view: window 
+          })
+        );
+
+        document.body.removeChild(downloadLink);
+      });
 
       if (bindFunctions) {
         bindFunctions(ctx.root);
