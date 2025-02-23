462b1cb04c4100c5dd8b7835058055db75c716a8
José Valim <jose.valim@dashbit.co>
2025-02-23 16:03:03 +0100

    Feedback

--- test/kino/screen_test.exs
+++ test/kino/screen_test.exs
@@ -3,37 +3,6 @@ defmodule Kino.ScreenTest do
 
   import Kino.Control
 
-  defmodule MyScreen do
-    def new(state) do
-      Kino.Screen.new(__MODULE__, state)
-    end
-
-    def render(state) do
-      state.()
-    end
-  end
-
-  defp control(button, parent) do
-    Kino.Screen.control(button, fn event, state ->
-      send(parent, {:control, event})
-      state
-    end)
-  end
-
-  defp screen_tree(frame) do
-    # Fetch screen supervision tree
-    assert_receive {:livebook_reference_object, sup, _}
-                   when is_pid(sup) and sup != self() and sup != frame.pid
-
-    [
-      {Kino.Screen.Watcher, watcher, :worker, _},
-      {DynamicSupervisor, dyn_sup, :supervisor, _}
-    ] = Supervisor.which_children(sup)
-
-    assert [] = DynamicSupervisor.which_children(dyn_sup)
-    {watcher, dyn_sup}
-  end
-
   test "renders" do
     _frame = MyScreen.new(fn -> "hello" end)
 
@@ -51,14 +20,9 @@ defmodule Kino.ScreenTest do
     assert_output(%{type: :frame_update, update: {:replace, [%{type: :control}]}})
     {watcher, dyn_sup} = screen_tree(frame)
 
-    # Click the button
-    info = %{origin: "client1"}
-    send(button.destination, {:event, button.ref, info})
-
-    assert_output_to("client1", %{
-      type: :frame_update,
-      update: {:replace, [%{type: :control}]}
-    })
+    # Verify a new user spawns a new workers
+    click_button(button, "client1")
+    assert_output_to("client1", %{type: :frame_update, update: {:replace, [%{type: :control}]}})
 
     assert [{_, user_screen, :worker, [Kino.Screen.Server]}] =
              DynamicSupervisor.which_children(dyn_sup)
@@ -71,44 +35,72 @@ defmodule Kino.ScreenTest do
   end
 
   test "does not crash tree when user event cannot be processed/rendered" do
-    button = button("hello")
+    ok_button = button("ok")
+    fail_event_button = button("fail_event")
+    fail_render_button = button("fail_render")
 
     frame =
       MyScreen.new(fn ->
-        Kino.Screen.control(button, fn
-          %{type: :succeed}, _ -> fn -> button end
-          %{type: :fail_event}, _ -> raise "oops"
-          %{type: :fail_render}, _ -> fn -> raise "oops" end
-        end)
+        Kino.Grid.new([
+          Kino.Screen.control(ok_button, fn _, _ -> fn -> ok_button end end),
+          Kino.Screen.control(fail_event_button, fn _, _ -> raise "oops" end),
+          Kino.Screen.control(fail_render_button, fn _, _ -> fn -> raise "oops" end end)
+        ])
       end)
 
     assert_output(%{type: :frame_update, update: {:replace, [%{type: :control}]}})
     {watcher, _dyn_sup} = screen_tree(frame)
 
     assert ExUnit.CaptureLog.capture_log(fn ->
-             # Click the button and make it fail
-             info = %{origin: "client1", type: :fail_event}
-             send(button.destination, {:event, button.ref, info})
-
-             # Click the button and make it succeed as a sync mechanism
-             info = %{origin: "client1", type: :succeed}
-             send(button.destination, {:event, button.ref, info})
+             click_button(fail_event_button, "client1")
+             click_button(ok_button, "client1")
              assert_output_to("client1", %{type: :frame_update})
            end) =~ "** (RuntimeError) oops"
 
     assert Process.alive?(watcher)
 
     assert ExUnit.CaptureLog.capture_log(fn ->
-             # Click the button and make it fail
-             info = %{origin: "client1", type: :fail_render}
-             send(button.destination, {:event, button.ref, info})
-
-             # Click the button and make it succeed as a sync mechanism
-             info = %{origin: "client1", type: :succeed}
-             send(button.destination, {:event, button.ref, info})
-             assert_output_to("client1", %{type: :frame_update})
+             click_button(fail_render_button, "client2")
+             click_button(ok_button, "client2")
+             assert_output_to("client2", %{type: :frame_update})
            end) =~ "** (RuntimeError) oops"
 
     assert Process.alive?(watcher)
+  end
+
+  defmodule MyScreen do
+    def new(rendering_fun) do
+      Kino.Screen.new(__MODULE__, rendering_fun)
+    end
+
+    def render(rendering_fun) do
+      rendering_fun.()
+    end
+  end
+
+  defp control(button, parent) do
+    Kino.Screen.control(button, fn event, state ->
+      send(parent, {:control, event})
+      state
+    end)
+  end
+
+  defp click_button(button, client) do
+    info = %{origin: client}
+    send(button.destination, {:event, button.ref, info})
+  end
+
+  defp screen_tree(frame) do
+    # Fetch screen supervision tree
+    assert_receive {:livebook_reference_object, sup, _}
+                   when is_pid(sup) and sup != self() and sup != frame.pid
+
+    [
+      {Kino.Screen.Watcher, watcher, :worker, _},
+      {DynamicSupervisor, dyn_sup, :supervisor, _}
+    ] = Supervisor.which_children(sup)
+
+    assert [] = DynamicSupervisor.which_children(dyn_sup)
+    {watcher, dyn_sup}
   end
 end
